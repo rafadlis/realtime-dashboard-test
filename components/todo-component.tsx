696cf64ec9955d4955a1aa1225b0638e50ab158b
rafadlis <rahmatfadlis@gmail.com>
2024-11-24 10:32:52 +0700

    chore: add better loading and error handling

--- components/todo-component.tsx
+++ components/todo-component.tsx
@@ -1,15 +1,14 @@
-"use client";
-
+import { Suspense } from "react";
 import { TodoForm } from "./todo-form";
 import { TodoList } from "./todo-list";
-import useSWR from "swr";
-import { getTodos } from "@/lib/get";
+
 export function TodoComponent() {
-  const { data } = useSWR("/api/todos", getTodos, { refreshInterval: 1 });
   return (
     <div>
       <TodoForm />
-      <TodoList data={data ?? []} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TodoList />
+      </Suspense>
     </div>
   );
 }
--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -1,9 +1,17 @@
-import { todos } from "@/drizzle/schema";
+"use client";
+import useSWR from "swr";
+import { getTodos } from "@/lib/get";
 
-export function TodoList({ data }: { data: (typeof todos.$inferSelect)[] }) {
+export function TodoList() {
+  const { data, isLoading, error } = useSWR("/api/todos", getTodos, {
+    refreshInterval: 1000,
+  });
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return null;
   return (
     <ul>
-      {data?.map((todo) => (
+      {data.map((todo) => (
         <li key={todo.id}>{todo.title}</li>
       ))}
     </ul>
